dedba457ea76c7026c2d859363e284a65cbd00ce
Prabir Das <dasprabir476@gmail.com>
2024-02-06 02:11:28 +0530

    practice codes

---
+++ Array part/array_1.py
@@ -0,0 +1,26 @@
+# list1=[1,2,"priya","batia",21,"vd"]
+# type(list1)
+# print(list1[2])
+#-----------------------------
+# tuple1=(2,3,45,5)
+# type(tuple1)
+# print(tuple1)
+#-----------------------------
+# set1={1,2,3,4,5}
+# type(set1)
+# print(set1[0])
+#---------------------------
+# list1=[1,2,4,5,6]
+# set2=set(list1)
+# #set2.add(15)
+# set2.clear()
+# print(set2)
+#--------------------Dic
+# dic1={"name":"su","email id":"su123@gmail.com","contact":"9641454408"}
+# type(dic1)
+# print(dic1["contact"])
+# dic1["address"]="abc colony"
+# print(dic1["address"])
+#--------------------------string
+
+
